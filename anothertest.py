c80586c7ad4ac012de1e5684f6401f17cdde95fb
Thibauld Savigny <twifun@outlook.fr>
2025-12-05 02:26:09 +0400

    rm anothertest.py

--- anothertest.py
+++
@@ -1,8 +0,0 @@
-# Use a pipeline as a high-level helper
-from transformers import pipeline
-
-pipe = pipeline("text-generation", model="google/gemma-3-1b-it")
-messages = [
-    {"role": "user", "content": "Who are you?"},
-]
-pipe(messages)
